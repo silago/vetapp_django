03239541f1249e3d3cd2fa524052752a269bf887
Ovchinnikov Alexander <aovchinnikov@2reallife.com>
2015-12-17 19:27:19 +0300

    basket models

--- base/templatetags/basket_info.py
+++ base/templatetags/basket_info.py
@@ -2,6 +2,8 @@ from django import template
 from django.contrib.auth.models import User
 from base.models import BasketProduct
 
+register = template.Library()
+
 @register.simple_tag(takes_context=True)
 def basket_info(context):
     uid = context['request'].user['user_id']
