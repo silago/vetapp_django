b32f2be96f3bb6d8637381f6dd9a906db0f7040e
silago <silago.nevermind@gmail.com>
2015-12-21 04:38:55 +0800

    pickle version

--- vetapp/settings.py
+++ vetapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import pickle
+pickle.HIGHTEST_PROTOCOL = 999
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
